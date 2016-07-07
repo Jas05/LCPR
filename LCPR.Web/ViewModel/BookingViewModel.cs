64726fd57aa3c08740bce4c74118125de07cc45b
Jas05 <Jasmeet05@gmail.com>
2016-07-07 14:07:14 +0100

    ViewModel and controller added

---
+++ LCPR.Web/ViewModel/BookingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LCPR.Web.ViewModel
+{
+    public class BookingViewModel
+    {
+        public string BookingRef { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
